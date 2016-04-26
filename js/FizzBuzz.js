8eba2a7616040e17812fac7a695035da5f857df0
Maggie <margaritakarlsson@yahoo.se>
2016-04-26 11:41:53 +0200

    The base is done. Rules are written. First assigment is not there jet.

---
+++ js/FizzBuzz.js
@@ -0,0 +1,35 @@
+/**
+ * Game: FIZZ BUZZ
+ * This is the actual playground. This is where we are going to write our code to make this game work.
+ * This page is one game. We will write every game in a new file.
+ *
+ * Our first game is going to be Fizz Buzz.
+ *
+ * RULES (Wikipedia):
+ * Fizz buzz is a group word game for children to teach them about division.
+ * Players take turns to count incrementally, replacing any number divisible by three with the word "fizz",
+ * and any number divisible by five with the word "buzz".
+ *
+ * The game is to be written in Javascript as the file suggests, jQuery and Angular are also allowed. Do not forget to make it simple.
+ *
+ * I'll start ^ ^ /PinkBambi
+ */
+
+$("#enterTheNumber").click(function() { //Finds the button and listens to a click.
+    //$('#answer').html($('input:text').val());   //Get the input and write it on page. Not needed but good to have as an example.
+    var playground = $('#playground').val(); //Get the input and aasign a variable.
+    /************ Do not change above code ************/
+
+
+
+    /************************/
+$("#answer").append(writeAnswer);
+});
+
+/*
+ * Bellow these lines is the space for problems/quests.
+ * Write down a problem/quest the same way I have done.
+ * This is so that we ceep track of what we have done so far. Don't forget to write this in chronical order.
+ * Just before these info-lines is the space for the solution code.
+ * I'll call the computer-anwers on the input numbers BERT. xD
+ */
